20733d563f24d2fe085ab88985e6e2a606d12630
AwaMelvine <melvineawa@gmail.com>
2019-11-08 08:37:54 +0100

    Create traversal graph

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -15,29 +15,55 @@ player = Player("Name", world.startingRoom)
 
 
 # FILL THIS IN
-traversalPath = ['n', 's']
+# traversalPath = ['n', 'n', 's']
+traversalPath = []
+traversalGraph = {}
 
-print(f"---{'n' in roomGraph[0][1].keys()}")
+for i in roomGraph:
+    traversalGraph[i] = roomGraph[i][1]
+print(traversalGraph)
 
-qq = Queue()
-qq.enqueue(0)
+# print(f"---{'n' in roomGraph[0][1].keys()}")
 
-while qq:
-    vertex = qq.dequeue()
-    if vertex in traversalPath:
-        continue
-    traversalPath.append(vertex)
-    print(vertex)
-    if vertex is not None:
-        for adj_vertex in roomGraph[vertex][1].keys():
-            # print(roomGraph[vertex][1][adj_vertex])
-            qq.enqueue(roomGraph[vertex][1][adj_vertex])
-    else:
-        break
+# qq = Queue()
+# qq.enqueue(0)
 
-print('---------------------------------|||||||\n\n')
-print(f'{traversalPath}')
-print('\n\n')
+# while qq:
+#     vertex = qq.dequeue()
+#     if vertex in traversalPath:
+#         continue
+#     traversalPath.append(vertex)
+#     print(vertex)
+#     if vertex is not None:
+#         for adj_vertex in roomGraph[vertex][1].keys():
+#             # print(roomGraph[vertex][1][adj_vertex])
+#             qq.enqueue(roomGraph[vertex][1][adj_vertex])
+#     else:
+#         break
+
+# stack = []
+# stack.append(3)
+
+# while stack:
+#     vertex = stack.pop()
+#     if vertex in traversalPath:
+#         continue
+
+#     print(vertex)
+#     if vertex is not None and vertex in roomGraph:
+#         traversalPath.append(vertex)
+#         print(vertex)
+#         for adj_vertex in roomGraph[vertex][1].keys():
+#             stack.append(roomGraph[vertex][1][adj_vertex])
+#     else:
+#         break
+
+# print('---------------------------------|||||||\n\n')
+# print(f'{traversalPath}')
+# print('\n\n')
+
+traversalPath = ['e', 'e', 'w', 'w', 'w', 'w',
+                 'e', 'e', 's', 's', 'n', 'n', 'n', 'n']
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -58,10 +84,10 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.currentRoom.printRoomDescription(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    else:
-        print("I did not understand that command.")
+# player.currentRoom.printRoomDescription(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     else:
+#         print("I did not understand that command.")
